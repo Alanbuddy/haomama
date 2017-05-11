18a9ea7d54b7b25e7a44bac2e8d29e5b70c389d2
alan <wenqiang@buddyrobots.com>
2017-05-11 18:05:13 +0800

    uu1

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,4 +12,11 @@ const { mix } = require('laravel-mix');
  */
 
 mix.js('resources/assets/js/app.js', 'public/js')
+	.webpackConfig({
+	        module: {
+	            rules: [
+	                { test: /\.coffee$/, loader: 'coffee-loader' }
+	            ]
+	        }
+	})
    .sass('resources/assets/sass/app.scss', 'public/css');
