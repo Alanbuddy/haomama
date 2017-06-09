e0e346a391dd20a92adb2c21ee7391a5bb108c26
alan <wenqiang@buddyrobots.com>
2017-06-09 17:05:27 +0800

    master change

---
+++ public/js/app.js
@@ -0,0 +1,219 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// identity function for calling harmony imports with the correct context
+/******/ 	__webpack_require__.i = function(value) { return value; };
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "./";
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 35);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */,
+/* 1 */
+/***/ (function(module, exports) {
+
+throw new Error("Module build failed: Error: ENOENT: no such file or directory, open '/home/alan/WxMpVod/resources/assets/js/app.coffee'\n\n");
+
+/***/ }),
+/* 2 */,
+/* 3 */,
+/* 4 */,
+/* 5 */,
+/* 6 */,
+/* 7 */,
+/* 8 */,
+/* 9 */,
+/* 10 */,
+/* 11 */,
+/* 12 */,
+/* 13 */,
+/* 14 */,
+/* 15 */,
+/* 16 */,
+/* 17 */,
+/* 18 */
+/***/ (function(module, exports) {
+
+throw new Error("Module build failed: ModuleBuildError: Module build failed: Error: ENOENT: no such file or directory, open '/home/alan/WxMpVod/resources/assets/sass/course-index.scss'\n\n\n    at runLoaders (/home/alan/WxMpVod/node_modules/webpack/lib/NormalModule.js:192:19)\n    at /home/alan/WxMpVod/node_modules/loader-runner/lib/LoaderRunner.js:364:11\n    at /home/alan/WxMpVod/node_modules/loader-runner/lib/LoaderRunner.js:200:19\n    at /home/alan/WxMpVod/node_modules/enhanced-resolve/lib/CachedInputFileSystem.js:62:14\n    at _combinedTickCallback (internal/process/next_tick.js:73:7)\n    at process._tickCallback (internal/process/next_tick.js:104:9)\n");
+
+/***/ }),
+/* 19 */
+/***/ (function(module, exports) {
+
+throw new Error("Module build failed: ModuleBuildError: Module build failed: Error: ENOENT: no such file or directory, open '/home/alan/WxMpVod/resources/assets/sass/setting-index.scss'\n\n\n    at runLoaders (/home/alan/WxMpVod/node_modules/webpack/lib/NormalModule.js:192:19)\n    at /home/alan/WxMpVod/node_modules/loader-runner/lib/LoaderRunner.js:364:11\n    at /home/alan/WxMpVod/node_modules/loader-runner/lib/LoaderRunner.js:200:19\n    at /home/alan/WxMpVod/node_modules/enhanced-resolve/lib/CachedInputFileSystem.js:62:14\n    at _combinedTickCallback (internal/process/next_tick.js:73:7)\n    at process._tickCallback (internal/process/next_tick.js:104:9)\n");
+
+/***/ }),
+/* 20 */
+/***/ (function(module, exports) {
+
+throw new Error("Module build failed: ModuleBuildError: Module build failed: Error: ENOENT: no such file or directory, open '/home/alan/WxMpVod/resources/assets/sass/setting-create.scss'\n\n\n    at runLoaders (/home/alan/WxMpVod/node_modules/webpack/lib/NormalModule.js:192:19)\n    at /home/alan/WxMpVod/node_modules/loader-runner/lib/LoaderRunner.js:364:11\n    at /home/alan/WxMpVod/node_modules/loader-runner/lib/LoaderRunner.js:200:19\n    at /home/alan/WxMpVod/node_modules/enhanced-resolve/lib/CachedInputFileSystem.js:62:14\n    at _combinedTickCallback (internal/process/next_tick.js:73:7)\n    at process._tickCallback (internal/process/next_tick.js:104:9)\n");
+
+/***/ }),
+/* 21 */
+/***/ (function(module, exports) {
+
+throw new Error("Module build failed: ModuleBuildError: Module build failed: Error: ENOENT: no such file or directory, open '/home/alan/WxMpVod/resources/assets/sass/mine-index.scss'\n\n\n    at runLoaders (/home/alan/WxMpVod/node_modules/webpack/lib/NormalModule.js:192:19)\n    at /home/alan/WxMpVod/node_modules/loader-runner/lib/LoaderRunner.js:364:11\n    at /home/alan/WxMpVod/node_modules/loader-runner/lib/LoaderRunner.js:200:19\n    at /home/alan/WxMpVod/node_modules/enhanced-resolve/lib/CachedInputFileSystem.js:62:14\n    at _combinedTickCallback (internal/process/next_tick.js:73:7)\n    at process._tickCallback (internal/process/next_tick.js:104:9)\n");
+
+/***/ }),
+/* 22 */
+/***/ (function(module, exports) {
+
+throw new Error("Module build failed: ModuleBuildError: Module build failed: Error: ENOENT: no such file or directory, open '/home/alan/WxMpVod/resources/assets/sass/mine-show.scss'\n\n\n    at runLoaders (/home/alan/WxMpVod/node_modules/webpack/lib/NormalModule.js:192:19)\n    at /home/alan/WxMpVod/node_modules/loader-runner/lib/LoaderRunner.js:364:11\n    at /home/alan/WxMpVod/node_modules/loader-runner/lib/LoaderRunner.js:200:19\n    at /home/alan/WxMpVod/node_modules/enhanced-resolve/lib/CachedInputFileSystem.js:62:14\n    at _combinedTickCallback (internal/process/next_tick.js:73:7)\n    at process._tickCallback (internal/process/next_tick.js:104:9)\n");
+
+/***/ }),
+/* 23 */
+/***/ (function(module, exports) {
+
+throw new Error("Module build failed: ModuleBuildError: Module build failed: Error: ENOENT: no such file or directory, open '/home/alan/WxMpVod/resources/assets/sass/course-create.scss'\n\n\n    at runLoaders (/home/alan/WxMpVod/node_modules/webpack/lib/NormalModule.js:192:19)\n    at /home/alan/WxMpVod/node_modules/loader-runner/lib/LoaderRunner.js:364:11\n    at /home/alan/WxMpVod/node_modules/loader-runner/lib/LoaderRunner.js:200:19\n    at /home/alan/WxMpVod/node_modules/enhanced-resolve/lib/CachedInputFileSystem.js:62:14\n    at _combinedTickCallback (internal/process/next_tick.js:73:7)\n    at process._tickCallback (internal/process/next_tick.js:104:9)\n");
+
+/***/ }),
+/* 24 */
+/***/ (function(module, exports) {
+
+throw new Error("Module build failed: ModuleBuildError: Module build failed: Error: ENOENT: no such file or directory, open '/home/alan/WxMpVod/resources/assets/sass/sign.scss'\n\n\n    at runLoaders (/home/alan/WxMpVod/node_modules/webpack/lib/NormalModule.js:192:19)\n    at /home/alan/WxMpVod/node_modules/loader-runner/lib/LoaderRunner.js:364:11\n    at /home/alan/WxMpVod/node_modules/loader-runner/lib/LoaderRunner.js:200:19\n    at /home/alan/WxMpVod/node_modules/enhanced-resolve/lib/CachedInputFileSystem.js:62:14\n    at _combinedTickCallback (internal/process/next_tick.js:73:7)\n    at process._tickCallback (internal/process/next_tick.js:104:9)\n");
+
+/***/ }),
+/* 25 */
+/***/ (function(module, exports) {
+
+throw new Error("Module build failed: ModuleBuildError: Module build failed: Error: ENOENT: no such file or directory, open '/home/alan/WxMpVod/resources/assets/sass/pay-success.scss'\n\n\n    at runLoaders (/home/alan/WxMpVod/node_modules/webpack/lib/NormalModule.js:192:19)\n    at /home/alan/WxMpVod/node_modules/loader-runner/lib/LoaderRunner.js:364:11\n    at /home/alan/WxMpVod/node_modules/loader-runner/lib/LoaderRunner.js:200:19\n    at /home/alan/WxMpVod/node_modules/enhanced-resolve/lib/CachedInputFileSystem.js:62:14\n    at _combinedTickCallback (internal/process/next_tick.js:73:7)\n    at process._tickCallback (internal/process/next_tick.js:104:9)\n");
+
+/***/ }),
+/* 26 */
+/***/ (function(module, exports) {
+
+throw new Error("Module build failed: ModuleBuildError: Module build failed: Error: ENOENT: no such file or directory, open '/home/alan/WxMpVod/resources/assets/sass/search-result.scss'\n\n\n    at runLoaders (/home/alan/WxMpVod/node_modules/webpack/lib/NormalModule.js:192:19)\n    at /home/alan/WxMpVod/node_modules/loader-runner/lib/LoaderRunner.js:364:11\n    at /home/alan/WxMpVod/node_modules/loader-runner/lib/LoaderRunner.js:200:19\n    at /home/alan/WxMpVod/node_modules/enhanced-resolve/lib/CachedInputFileSystem.js:62:14\n    at _combinedTickCallback (internal/process/next_tick.js:73:7)\n    at process._tickCallback (internal/process/next_tick.js:104:9)\n");
+
+/***/ }),
+/* 27 */
+/***/ (function(module, exports) {
+
+throw new Error("Module build failed: ModuleBuildError: Module build failed: Error: ENOENT: no such file or directory, open '/home/alan/WxMpVod/resources/assets/sass/message.scss'\n\n\n    at runLoaders (/home/alan/WxMpVod/node_modules/webpack/lib/NormalModule.js:192:19)\n    at /home/alan/WxMpVod/node_modules/loader-runner/lib/LoaderRunner.js:364:11\n    at /home/alan/WxMpVod/node_modules/loader-runner/lib/LoaderRunner.js:200:19\n    at /home/alan/WxMpVod/node_modules/enhanced-resolve/lib/CachedInputFileSystem.js:62:14\n    at _combinedTickCallback (internal/process/next_tick.js:73:7)\n    at process._tickCallback (internal/process/next_tick.js:104:9)\n");
+
+/***/ }),
+/* 28 */
+/***/ (function(module, exports) {
+
+throw new Error("Module build failed: ModuleBuildError: Module build failed: Error: ENOENT: no such file or directory, open '/home/alan/WxMpVod/resources/assets/sass/course-show.scss'\n\n\n    at runLoaders (/home/alan/WxMpVod/node_modules/webpack/lib/NormalModule.js:192:19)\n    at /home/alan/WxMpVod/node_modules/loader-runner/lib/LoaderRunner.js:364:11\n    at /home/alan/WxMpVod/node_modules/loader-runner/lib/LoaderRunner.js:200:19\n    at /home/alan/WxMpVod/node_modules/enhanced-resolve/lib/CachedInputFileSystem.js:62:14\n    at _combinedTickCallback (internal/process/next_tick.js:73:7)\n    at process._tickCallback (internal/process/next_tick.js:104:9)\n");
+
+/***/ }),
+/* 29 */
+/***/ (function(module, exports) {
+
+throw new Error("Module build failed: ModuleBuildError: Module build failed: Error: ENOENT: no such file or directory, open '/home/alan/WxMpVod/resources/assets/sass/teacher.scss'\n\n\n    at runLoaders (/home/alan/WxMpVod/node_modules/webpack/lib/NormalModule.js:192:19)\n    at /home/alan/WxMpVod/node_modules/loader-runner/lib/LoaderRunner.js:364:11\n    at /home/alan/WxMpVod/node_modules/loader-runner/lib/LoaderRunner.js:200:19\n    at /home/alan/WxMpVod/node_modules/enhanced-resolve/lib/CachedInputFileSystem.js:62:14\n    at _combinedTickCallback (internal/process/next_tick.js:73:7)\n    at process._tickCallback (internal/process/next_tick.js:104:9)\n");
+
+/***/ }),
+/* 30 */
+/***/ (function(module, exports) {
+
+throw new Error("Module build failed: ModuleBuildError: Module build failed: Error: ENOENT: no such file or directory, open '/home/alan/WxMpVod/resources/assets/sass/creview.scss'\n\n\n    at runLoaders (/home/alan/WxMpVod/node_modules/webpack/lib/NormalModule.js:192:19)\n    at /home/alan/WxMpVod/node_modules/loader-runner/lib/LoaderRunner.js:364:11\n    at /home/alan/WxMpVod/node_modules/loader-runner/lib/LoaderRunner.js:200:19\n    at /home/alan/WxMpVod/node_modules/enhanced-resolve/lib/CachedInputFileSystem.js:62:14\n    at _combinedTickCallback (internal/process/next_tick.js:73:7)\n    at process._tickCallback (internal/process/next_tick.js:104:9)\n");
+
+/***/ }),
+/* 31 */
+/***/ (function(module, exports) {
+
+throw new Error("Module build failed: ModuleBuildError: Module build failed: Error: ENOENT: no such file or directory, open '/home/alan/WxMpVod/resources/assets/sass/mycourse.scss'\n\n\n    at runLoaders (/home/alan/WxMpVod/node_modules/webpack/lib/NormalModule.js:192:19)\n    at /home/alan/WxMpVod/node_modules/loader-runner/lib/LoaderRunner.js:364:11\n    at /home/alan/WxMpVod/node_modules/loader-runner/lib/LoaderRunner.js:200:19\n    at /home/alan/WxMpVod/node_modules/enhanced-resolve/lib/CachedInputFileSystem.js:62:14\n    at _combinedTickCallback (internal/process/next_tick.js:73:7)\n    at process._tickCallback (internal/process/next_tick.js:104:9)\n");
+
+/***/ }),
+/* 32 */
+/***/ (function(module, exports) {
+
+throw new Error("Module build failed: ModuleBuildError: Module build failed: \n@import \"node_modules/bootstrap-sass/assets/stylesheets/bootstrap\";\n^\n      File to import not found or unreadable: node_modules/bootstrap-sass/assets/stylesheets/bootstrap.\nParent style sheet: stdin\n      in /home/alan/WxMpVod/resources/assets/sass/app.scss (line 9, column 1)\n    at runLoaders (/home/alan/WxMpVod/node_modules/webpack/lib/NormalModule.js:192:19)\n    at /home/alan/WxMpVod/node_modules/loader-runner/lib/LoaderRunner.js:364:11\n    at /home/alan/WxMpVod/node_modules/loader-runner/lib/LoaderRunner.js:230:18\n    at context.callback (/home/alan/WxMpVod/node_modules/loader-runner/lib/LoaderRunner.js:111:13)\n    at Object.asyncSassJobQueue.push [as callback] (/home/alan/WxMpVod/node_modules/sass-loader/lib/loader.js:51:13)\n    at Object.<anonymous> (/home/alan/WxMpVod/node_modules/sass-loader/node_modules/async/dist/async.js:2243:31)\n    at Object.callback (/home/alan/WxMpVod/node_modules/sass-loader/node_modules/async/dist/async.js:906:16)\n    at options.error (/home/alan/WxMpVod/node_modules/node-sass/lib/index.js:294:32)\n");
+
+/***/ }),
+/* 33 */
+/***/ (function(module, exports) {
+
+throw new Error("Module build failed: ModuleBuildError: Module build failed: Error: ENOENT: no such file or directory, open '/home/alan/WxMpVod/resources/assets/sass/layout.scss'\n\n\n    at runLoaders (/home/alan/WxMpVod/node_modules/webpack/lib/NormalModule.js:192:19)\n    at /home/alan/WxMpVod/node_modules/loader-runner/lib/LoaderRunner.js:364:11\n    at /home/alan/WxMpVod/node_modules/loader-runner/lib/LoaderRunner.js:200:19\n    at /home/alan/WxMpVod/node_modules/enhanced-resolve/lib/CachedInputFileSystem.js:62:14\n    at _combinedTickCallback (internal/process/next_tick.js:73:7)\n    at process._tickCallback (internal/process/next_tick.js:104:9)\n");
+
+/***/ }),
+/* 34 */
+/***/ (function(module, exports) {
+
+throw new Error("Module build failed: ModuleBuildError: Module build failed: Error: ENOENT: no such file or directory, open '/home/alan/WxMpVod/resources/assets/sass/register.scss'\n\n\n    at runLoaders (/home/alan/WxMpVod/node_modules/webpack/lib/NormalModule.js:192:19)\n    at /home/alan/WxMpVod/node_modules/loader-runner/lib/LoaderRunner.js:364:11\n    at /home/alan/WxMpVod/node_modules/loader-runner/lib/LoaderRunner.js:200:19\n    at /home/alan/WxMpVod/node_modules/enhanced-resolve/lib/CachedInputFileSystem.js:62:14\n    at _combinedTickCallback (internal/process/next_tick.js:73:7)\n    at process._tickCallback (internal/process/next_tick.js:104:9)\n");
+
+/***/ }),
+/* 35 */
+/***/ (function(module, exports, __webpack_require__) {
+
+__webpack_require__(1);
+__webpack_require__(33);
+__webpack_require__(34);
+__webpack_require__(18);
+__webpack_require__(19);
+__webpack_require__(20);
+__webpack_require__(21);
+__webpack_require__(22);
+__webpack_require__(23);
+__webpack_require__(24);
+__webpack_require__(25);
+__webpack_require__(26);
+__webpack_require__(27);
+__webpack_require__(28);
+__webpack_require__(29);
+__webpack_require__(30);
+__webpack_require__(31);
+module.exports = __webpack_require__(32);
+
+
+/***/ })
+/******/ ]);
