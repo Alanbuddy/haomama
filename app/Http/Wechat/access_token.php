5ab091acbc9d5bb0f91a7449757be4f04ba00868
gao <fishis@163.com>
2017-06-13 09:58:25 +0800

    clean

--- app/Http/Wechat/access_token.php
+++
@@ -1,1 +0,0 @@
-<?php exit();?>{"access_token":"YUJqcVmU3o88CU6EdB8wlvtFsAOKVGhRI6_Yem9vOsg5PSVEv3RB2EXlt7sv3YlgQPLqRt_KNmyKH0YnXpXcS2rlp1T9TpDzbI2Dy_BvBTf4ldJo23j6eI1BEjz1NiFuMHNdAIAWRX","expire_time":1497256590}
--- app/Http/Wechat/jsapi_ticket.php
+++
@@ -1,1 +0,0 @@
-<?php exit();?>{"jsapi_ticket":"bxLdikRXVbTPdHSM05e5uycGLiSRLEl2z1i5SVviBCOA2rsRk-gMJeN1Sxvp8P3WyoBVquesPXkIunjlxNVgxQ","expire_time":1497256591}
