e1d8c4d3dd2a580fad5b91e11793029ce5b43155
gao <fishis@163.com>
2017-06-14 11:29:21 +0800

    c

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -127,6 +127,7 @@ class UserController extends Controller
     public function profile()
     {
         $user=auth()->user();
+        $user->baby=json_decode($user->baby);
         return view('setting.index',['user'=>$user]);
     }
 }
--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -11,10 +11,26 @@ class UserTableSeeder extends Seeder
      */
     public function run()
     {
+//        DB::table('users')->insert([
+//            'name' => 'aj',
+//            'email' => 'fishis@163.com',
+//            'password' => bcrypt('1234'),
+//            'baby' => json_encode([[
+//                'name' => str_random(3),
+//                'gender' => 'male',
+//                'birthday' => date('Y-m-d', strtotime('-1 month'))],
+//            ])
+//        ]);
         DB::table('users')->insert([
-            'name' => 'aj',
-            'email' => 'fishis@163.com',
-            'password' => bcrypt('1234'),
-        ]);
+                'name' => str_random(4),
+                'email' => str_random(5) . '@163.com',
+                'password' => bcrypt('1234'),
+                'baby' => json_encode([[
+                    'name' => str_random(3),
+                    'gender' => 'male',
+                    'birthday' => date('Y-m-d', strtotime('-1 month'))],
+                ])
+            ]
+        );
     }
 }
