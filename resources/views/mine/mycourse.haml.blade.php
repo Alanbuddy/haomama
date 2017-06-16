2fe0e62efb875438913d8c90a79131e0bc55f7f8
alan <wenqiang@buddyrobots.com>
2017-06-16 15:09:47 +0800

    search result3

--- resources/views/mine/mycourse.haml.blade.php
+++ resources/views/mine/mycourse.haml.blade.php
@@ -9,35 +9,35 @@
   %p.fb.tc.fb.color7.f18 我的课程
   %img.back{src: "/icon/back.png"}
   %hr.div-line
-- if (!$items)
+- if (count($items) == 0)
   .empty-div
     %img.empty-icon{src: "/icon/empty.png"}
     %p.empty-message.f12.color5 您还没有相关课程信息......
-.course-item-div
-  - foreach ($items as $item)
-    .course-item
-      .course-icon-div
-        %img.course-icon{src: $item['cover'] ? $item['cover'] : "/icon/example.png"}
-      .word-div
-        .course-row-div.clearfix
-          %span.f12.category-class= $item['category']['name']
-          %span.course-item-value.f14.color5= "￥". $item['price']
-        .course-row-div.color7
-          - if ($item['type'] == 'offline')
-            %span.name-span.f16= $item['name']
-            %span.course-status.f8 线下
-          - else 
-            %span.course-name.f16= $item['name']
-        .course-row-div.f12.color6
-          - if ($item['type'] == "offline")
-            %span.participate= $item['users_count']."人已报名"
-            %span .
-            %span= date_format(date_create($item['begin']),"m月/d日")."开课"
-          - else
-            %span.participate= $item['users_count']."人已学"
-            %span .
-            %span= $item['comments_count'] ."条评论"
-
+- else
+  .course-item-div
+    - foreach ($items as $item)
+      .course-item
+        .course-icon-div
+          %img.course-icon{src: $item['cover'] ? $item['cover'] : "/icon/example.png"}
+        .word-div
+          .course-row-div.clearfix
+            %span.f12.category-class= $item['category']['name']
+            %span.course-item-value.f14.color5= "￥". $item['price']
+          .course-row-div.color7
+            - if ($item['type'] == 'offline')
+              %span.name-span.f16= $item['name']
+              %span.course-status.f8 线下
+            - else 
+              %span.course-name.f16= $item['name']
+          .course-row-div.f12.color6
+            - if ($item['type'] == "offline")
+              %span.participate= $item['users_count']."人已报名"
+              %span .
+              %span= date_format(date_create($item['begin']),"m月/d日")."开课"
+            - else
+              %span.participate= $item['users_count']."人已学"
+              %span .
+              %span= $item['comments_count'] ."条评论"
 @endsection
 
 
