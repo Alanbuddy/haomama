06329c4d172bdb7a302c9ee8c717b1bf1a90e3df
gao <fishis@163.com>
2017-06-16 15:07:19 +0800

    merge

--- app/Http/Controllers/CourseController.php
+++ app/Http/Controllers/CourseController.php
@@ -89,7 +89,7 @@ class CourseController extends Controller
         $enrolledCount = $this->enrolledCount($course);
         $favoritedCount = $this->favoritedCount($course);
         $recommendedCourses = Search::recommend($course)->keys()->all();
-        return view('admin.course.show',
+        return view('course.show',//'admin.course.show',
             compact('course',
                 'hasEnrolled',
                 'hasFavorited',
