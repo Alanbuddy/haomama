0d593c946dcf1c1d2d3b06bc36b2d8d2ac7456e4
alan <wenqiang@buddyrobots.com>
2017-06-16 09:31:34 +0800

    1

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -76,7 +76,7 @@ class HomeController extends Controller
 
         $jsSdk = new JSSDK(config('wechat.mp.app_id'), config('wechat.mp.app_secret'));
         $signPackage = $jsSdk->getSignPackage();
-
+        // dd($data);
         return view('course.index',
             compact('categories', 'data ', 'signPackage')
         );
