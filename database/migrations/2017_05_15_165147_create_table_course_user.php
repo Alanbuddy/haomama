7eebf781a4fcfb02178a337150039e5d5a37e9dd
gao <fishis@163.com>
2017-06-19 14:40:03 +0800

    merge

--- database/migrations/2017_05_15_165147_create_table_course_user.php
+++ database/migrations/2017_05_15_165147_create_table_course_user.php
@@ -17,14 +17,14 @@ class CreateTableCourseUser extends Migration
             $table->integer('user_id')->unsigned();
             $table->integer('course_id')->unsigned();
             $table->enum('user_type', ['', 'student', 'teacher']);
-            $table->enum('type', ['enroll', 'favorite']);
+            $table->enum('type', ['enroll', 'favorite','teach']);
 
             $table->foreign('user_id')->references('id')->on('users')
                 ->onUpdate('cascade')->onDelete('cascade');
             $table->foreign('course_id')->references('id')->on('courses')
                 ->onUpdate('cascade')->onDelete('cascade');
 
-            $table->primary(['course_id', 'user_id', 'type']);
+            $table->primary(['course_id', 'user_id', 'type','user_type']);
         });
     }
 
