14057c63fa43036696686a264b7af84ddf7f9981
alan <wenqiang@buddyrobots.com>
2017-06-23 13:43:11 +0800

    message1

--- resources/views/setting/message.haml.blade.php
+++ resources/views/setting/message.haml.blade.php
@@ -8,25 +8,30 @@
   %p.fb.tc.fb.color7.f18 消息
   %img.back{src: "/icon/back.png"}
   %hr.div-line
-.empty-div
-  %img.empty-icon{src: "/icon/empty.png"}
-  %p.empty-message.f12.color5 您还没有相关消息......
-.message-item-div
-  .item-review
-    .avatar-div
-      %img.avatar{src: "/icon/avatar.png"}
-    .desc-div
-      %p.name.f12.color7 路人甲
-      %p.time.f12.color5 三天前
-      %p.num.f14.color7 等9人赞了您的评论
-      %p.notice.f14.color5 "本课程提供重要知识点备忘课件，课件下载地址为：......."
-  .item-notice
-    .avatar-div
-      %img.avatar{src: "/icon/avatar.png"}
-    .desc-div
-      %p.name.f12.color7 课程小助手
-      %p.time.f12.color5 三天前
-      %p.num.f14.color7 您报名的课程"名字很长很长的"已有更新～请尽快学习～
+- if (count($messages) > 0)
+  .message-item-div
+    - foreach ($messages as $message)
+      - if ($message['from'])
+        .item-review
+          .avatar-div
+            %img.avatar{src: $message['avatar'] ? $message['avatar'] : "/icon/avatar.png"}
+          .desc-div
+            %p.name.f12.color7= $message['name']
+            %p.time.f12.color5= (strtotime(time()) - strtotime($message['created_at']))."天前"
+            %p.num.f14.color7= "等".$message['num']."人赞了您的评论"
+            %p.notice.f14.color5= $message['content']
+      - if (!$message['from'])
+        .item-notice
+          .avatar-div
+            %img.avatar{src: "/icon/ma_icon.png"}
+          .desc-div
+            %p.name.f12.color7 课程小助手
+            %p.time.f12.color5= (strtotime(time()) - strtotime($message['created_at']))."天前"
+            %p.num.f14.color7= $message['content']
+- else
+  .empty-div
+    %img.empty-icon{src: "/icon/empty.png"}
+    %p.empty-message.f12.color5 您还没有相关消息......
 
 @endsection
 
