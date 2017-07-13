74fee4fab1ebb39a6a671ece04a2777f6fcc2274
alan <wenqiang@buddyrobots.com>
2017-07-13 11:35:41 +0800

    test upload

--- public/js/fileupload.js
+++ public/js/fileupload.js
@@ -1,62 +1,64 @@
 $(document).ready(function(){
-  $('#upload').html5uploader({
+  var $list = $("#thelist");
+  var $btn = $('#ctlBtn');
+  var uploader = WebUploader.create({
 
-    auto:false,
+      // swf文件路径
+    swf: '/js/plugin/Uploader.swf',
 
-    multi:true,
+    // 文件接收服务端。
+    server: "/haml?m=upload",
 
-    removeTimeout:9999999,
+    // 选择文件的按钮。可选。
+    // 内部根据当前运行是创建，可能是input元素，也可能是flash.
+    pick: '#picker',
 
-    url:window.video,
+    // 不压缩image, 默认如果是jpeg，文件上传前会压缩一把再上传！
+    resize: false,
+    auto: false,
+    method: 'POST'       //是否自动上传,true为自动上传
+    // chunked: true,     //是否要分片处理大文件上传
+    // chunkSize: 5*1024*1024    //分片上传，每片2M，默认是5M
+  });
+  uploader.on( 'fileQueued', function( file ) {
+    $list.append( '<div id="' + file.id + '" class="item">' +
+        '<h4 class="info">' + file.name + '</h4>' +
+        '<p class="state">等待上传...</p>' +
+    '</div>' );
+  });
 
-    onUploadStart:function(){
+  uploader.on( 'uploadProgress', function( file, percentage ) {
+    var $li = $( '#'+file.id ),
+      $percent = $li.find('.progress .progress-bar');
 
-      alert('开始上传');
+    // 避免重复创建
+    if ( !$percent.length ) {
+      $percent = $('<div class="progress progress-striped active">' +
+        '<div class="progress-bar" role="progressbar" style="width: 0%">' +
+        '</div>' +
+      '</div>').appendTo( $li ).find('.progress-bar');
+    }
 
-      },
+    $li.find('p.state').text('上传中');
 
-    onInit:function(){
+    $percent.css( 'width', percentage * 100 + '%' );
+  });
 
-      // alert('初始化');
+  uploader.on( 'uploadSuccess', function( file ) {
+    $( '#'+file.id ).addClass('upload-state-done');
+    $( '#'+file.id ).find('p.state').text('已上传');
+  });
 
-      },
+  uploader.on( 'uploadError', function( file ) {
+    $( '#'+file.id ).find('p.state').text('上传失败');
+  });
 
-    onUploadComplete:function(){
+  uploader.on( 'uploadComplete', function( file ) {
+    $( '#'+file.id ).find('.progress').fadeOut();
+  });
 
-      alert('上传完成');
-
-      }
-
-    });
-
-  $('#upload_img').html5uploader({
-
-    auto:false,
-
-    multi:true,
-
-    removeTimeout:9999999,
-
-    url:window.video,
-
-    onUploadStart:function(){
-
-      alert('开始上传');
-
-      },
-
-    onInit:function(){
-
-      // alert('初始化');
-
-      },
-
-    onUploadComplete:function(){
-
-      alert('上传完成');
-
-      }
-
-    });
+  $btn.click(function(){
+    uploader.upload();
+  });
 
 });
