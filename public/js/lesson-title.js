5bc979915891a5ca6d3be7cfd51af36f3e86eaa4
alan <wenqiang@buddyrobots.com>
2017-07-19 17:01:04 +0800

    course store

--- public/js/lesson-title.js
+++ public/js/lesson-title.js
@@ -1,26 +1,45 @@
 $(document).ready(function(){
+  var total = 0;
+  var pageIndex = 0;     //页面索引初始值   
+  var pageSize = 10;     //每页显示条数初始化，修改显示条数，修改这里即可
   $(".addlesson").click(function(){
-    $.getJSON(
-      window.lessons_index,
-      {},
-      function(data){
-        console.log(data.per_page);
-        for(var i=0;i<data.total;i++){
-          var check_item = $('<div class="checkbox">' +
-                              '<label>' +
-                                '<input type="checkbox" name="lesson-check" value=' + data.data[i].id + '>' + data.data[i].name +
-                              '</label>' +
-                            '</div>');
-          $(".checkbox-items").append(check_item);
-        }
-        var select_page = $('<div class="select-page">' +
-                              '<span class="totalitems">' + "共" + data.total/data.per_page + "页,总计" + data.total + "条" + '</span>' +
-                            '</div>');
-        $(".checkbox-items").append(select_page);
-      }
-      );
+    InitTable(0); 
+    function PageCallback(index, jq) {  
+      InitTable(index);  
+      return false;
+    }  
+    function InitTable(pageIndex) {                                  
+      $.ajax({   
+          type: "get",  
+          url: window.lessons_index,      //提交到一般处理程序请求数据   
+          data: "page=" + (pageIndex + 1),          //提交两个参数：pageIndex(页面索引)，pageSize(显示条数)                   
+          success: function(data) {
+            total = data.total;
+            $(".checkbox-items .checkbox").remove();
+            for(var i=0;i<data.per_page;i++){
+              var check_item = $('<div class="checkbox">' +
+                                    '<label>' +
+                                      '<input type="checkbox" name="lesson-check" value=' + data.data[i].id + ' data-text=' + data.data[i].name + '>' + data.data[i].name +
+                                    '</label>' +
+                                '</div>');
+              $(".checkbox-items").append(check_item);
+            }
+            $("#Pagination").pagination(total, {
+              callback: PageCallback,  //PageCallback() 为翻页调用次函数。
+              prev_text: "«上一页",
+              next_text: "»下一页",
+              items_per_page: pageSize,
+              num_edge_entries: 2,       //两侧首尾分页条目数
+              num_display_entries: 4,    //连续分页主体部分分页条目数
+              current_page: pageIndex,   //当前页索引
+              link_to: "",
+            });
+          } 
+      });
+    }
     $("#lessonModal").modal("show");
   });
+  
 
   var type = "tag";
   $('#type-tag').tagEditor({
@@ -164,12 +183,12 @@ $(document).ready(function(){
   };
   editor_lesson.create();
 
-  $("#confirm-btn").click(function(){
+  $(document).on('click', '#confirm-btn', function(){
     var title_arr = [];
     var title_text = [];
     $("[name='lesson-check']:input:checked").each(function(){
         var value = $(this).val();
-        var text = $(this).text();
+        var text = $(this).attr("data-text");
         title_arr.push(value);
         title_text.push(text);
     });
@@ -209,6 +228,16 @@ $(document).ready(function(){
     $("#shelfModal").modal("hide");
   });
   
+  function check_input(name, length, ori_price, price){
+    if(name == ""){
+      showMsg("课程名称必须填写", "center");
+      return false;
+    }
+    if(!$.isNumeric(length) || !$.isNumeric(ori_price) || !$.isNumeric(price)){
+      showMsg("请输入正确的数字", "center");
+      return false;
+    }
+  }
 
   $(document).on('click',"#finish-btn", function(){
     var name = $("#course-name").val().trim();
@@ -223,7 +252,7 @@ $(document).ready(function(){
     var desc = editor.txt.html();
     var lesson_list = [];
     $(".example li").each(function(){
-      lesson_list.push($(this).text());
+      lesson_list.push($(this).attr("data-id"));
     });
     var lesson_title = editor_lesson.txt.html();
     var teacher_arr = [];
@@ -231,6 +260,10 @@ $(document).ready(function(){
       teacher_arr.push($(this).text());
     });
     var path = $(".cover-path").text();
+    var ret = check_input(name, length, original_price, price);
+    if(ret == false) {
+      return false;
+    }
     $.postJSON(
       window.course_store,
       {
