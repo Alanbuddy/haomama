a3103a5fc5c73bc0ca39c73f4a59c7faaf48ea59
gao <fishis@163.com>
2017-07-27 17:27:48 +0800

    merge

--- public/a.php
+++ public/a.php
@@ -5,5 +5,5 @@ echo $cmd;
 //echo shell_exec('tail /tmp/supervisord.log');
 //echo shell_exec($cmd);
 
-echo 'Executing: git commit -m "merge" -a';
+echo 'Executing: git commit -m "merge" -a'."\n";
 echo shell_exec('git commit -m "merge" -a');
