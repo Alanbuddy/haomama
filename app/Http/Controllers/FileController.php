e5f8b23fd75e61ab2d8fb7151318489440b0eeb4
gao <fishis@163.com>
2017-08-03 17:15:15 +0800

    merge

--- app/Http/Controllers/FileController.php
+++ app/Http/Controllers/FileController.php
@@ -30,7 +30,7 @@ class FileController extends Controller
         $item->fill($this->getFileBaseInfo($file));
         $item->save();
         $path = substr($path->getPathName(), strlen(public_path()));
-        return env('APP_URL').$path;
+        return ['errno' => 0, 'data' => env('APP_URL') . $path];
         return ['success' => true, 'path' => $path, 'data' => $item];
     }
 
