f08f5216a5e8431543e0fb667854b1a51bf49385
gao <fishis@163.com>
2017-08-10 16:29:59 +0800

    merge

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -10,14 +10,19 @@ class RedirectIfAuthenticated
     /**
      * Handle an incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
-     * @param  string|null  $guard
+     * @param  \Illuminate\Http\Request $request
+     * @param  \Closure $next
+     * @param  string|null $guard
      * @return mixed
      */
     public function handle($request, Closure $next, $guard = null)
     {
         if (Auth::guard($guard)->check()) {
+            if (auth()->user()->hasRole('admin')
+                || auth()->user()->hasRole('operator')
+            ) {
+                return redirect()->route('courses.index');
+            }
             return redirect('/');
         }
 
