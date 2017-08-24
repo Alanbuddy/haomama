367e1031ba220a892be9f30187ae15d3782414c4
gao <fishis@163.com>
2017-08-24 09:47:43 +0800

    merge

--- resources/views/errors/403.blade.php
+++ resources/views/errors/403.blade.php
@@ -2,7 +2,7 @@
 <html>
 <head>
   <base href={{$base_href}}>
-  <title>404错误</title>
+  <title>权限不足</title>
   <meta name="viewport" content="width=device-width,initial-scale=1">
   <style>
   html, body {
