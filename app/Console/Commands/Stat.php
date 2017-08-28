e60e70a4b65ea10d634f03f9ac19d161f65aa1fa
gao <fishis@163.com>
2017-08-28 14:30:50 +0800

    merge

--- app/Console/Commands/Stat.php
+++ app/Console/Commands/Stat.php
@@ -3,6 +3,8 @@
 namespace App\Console\Commands;
 
 use App\Http\Wechat\WxApi;
+use App\Models\User;
+use App\Statistic;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\DB;
 
@@ -39,21 +41,25 @@ class Stat extends Command
      */
     public function handle()
     {
+        $subscribers = [];
         DB::table('users')
             ->whereNotNull('openid')
             ->orderBy('id')
-            ->chunk(100, function ($users) {
+            ->chunk(100, function ($users) use (&$subscribers) {
                 foreach ($users as $user) {
-                    $result=WxApi::commonUserInfo($user->openid);
+                    $result = WxApi::commonUserInfo($user->openid);
 //                    $this->info(property_exists(json_decode($result['data']),'subscribe')?1:0);
-                    if(property_exists(json_decode($result['data']),'subscribe')){
-                        $this->info('User '.$user->id.' 已经关注');
-//                        $this->info(json_encode($result));
-                    }else{
-                        $this->info('User '.$user->id.' 未关注');
+                    if (property_exists(json_decode($result['data']), 'subscribe')) {
+                        $subscribers[] = $user->id;
+                        $this->info('User ' . $user->id . ' 已经关注');
+                        $this->info(json_encode($subscribers));
+                    } else {
+                        $this->info('User ' . $user->id . ' 未关注');
                     }
                 }
             });
+        User::whereIn('id', $subscribers)->update(['subscribe' => true]);
+        Statistic::create(['type'=>'subscriber','data'=>$subscribers->count()]);
         $this->info('aa');
     }
 }
