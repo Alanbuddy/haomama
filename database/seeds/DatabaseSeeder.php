ba979024896c4873a9e3c3c021de76857e7b7042
aj <bitmyth@outlook.com>
2017-09-30 14:01:50 +0800

    merge

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,25 +11,25 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(UserTableSeeder::class);
+//        $this->call(UserTableSeeder::class);
         $this->call(EntrustSeeder::class);
         $this->call(SettingSeeder::class);
         $this->call(TagSeeder::class);
-//        $this->call(LessonSeeder::class);
-//        $this->call(CourseSeeder::class);
-//        $this->call(CourseUserSeeder::class);
-//        $this->call(CommentSeeder::class);
-//        DB::table('term_object')->insert(['term_id' => 1, 'object_id' => 1, 'type' => 'tag',]);
-//        DB::table('term_object')->insert(['term_id' => 2, 'object_id' => 1, 'type' => 'tag',]);
-//        DB::table('term_object')->insert(['term_id' => 3, 'object_id' => 1, 'type' => 'tag',]);
-//        DB::table('term_object')->insert(['term_id' => 3, 'object_id' => 2, 'type' => 'tag',]);
-//        DB::table('term_object')->insert(['term_id' => 1, 'object_id' => 2, 'type' => 'tag',]);
-//        DB::table('term_object')->insert(['term_id' => 2, 'object_id' => 3, 'type' => 'tag',]);
-//        DB::table('term_object')->insert(['term_id' => 1, 'object_id' => 4, 'type' => 'tag',]);
-//        DB::table('term_object')->insert(['term_id' => 1, 'object_id' => 5, 'type' => 'tag',]);
-//        DB::table('term_object')->insert(['term_id' => 2, 'object_id' => 5, 'type' => 'tag',]);
-//        DB::table('term_object')->insert(['term_id' => 3, 'object_id' => 5, 'type' => 'tag',]);
-//        $this->call(VoteSeeder::class);
+        $this->call(LessonSeeder::class);
+        $this->call(CourseSeeder::class);
+        $this->call(CourseUserSeeder::class);
+        $this->call(CommentSeeder::class);
+        DB::table('term_object')->insert(['term_id' => 1, 'object_id' => 1, 'type' => 'tag',]);
+        DB::table('term_object')->insert(['term_id' => 2, 'object_id' => 1, 'type' => 'tag',]);
+        DB::table('term_object')->insert(['term_id' => 3, 'object_id' => 1, 'type' => 'tag',]);
+        DB::table('term_object')->insert(['term_id' => 3, 'object_id' => 2, 'type' => 'tag',]);
+        DB::table('term_object')->insert(['term_id' => 1, 'object_id' => 2, 'type' => 'tag',]);
+        DB::table('term_object')->insert(['term_id' => 2, 'object_id' => 3, 'type' => 'tag',]);
+        DB::table('term_object')->insert(['term_id' => 1, 'object_id' => 4, 'type' => 'tag',]);
+        DB::table('term_object')->insert(['term_id' => 1, 'object_id' => 5, 'type' => 'tag',]);
+        DB::table('term_object')->insert(['term_id' => 2, 'object_id' => 5, 'type' => 'tag',]);
+        DB::table('term_object')->insert(['term_id' => 3, 'object_id' => 5, 'type' => 'tag',]);
+        $this->call(VoteSeeder::class);
 
     }
 }
